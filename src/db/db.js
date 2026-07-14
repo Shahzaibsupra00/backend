f6851511170c29b98e510f1ec1bb25eaf3410c3e
Shahzaib Hassan <janisupra00@gmail.com>
2026-07-14 10:23:47 +0500

    baisc changes

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("Connected to MongoDB");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
